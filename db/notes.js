293aee288f80ea388baa6f0215c950f357c54069
Sterling Peck <peck.sterling@gmail.com>
2022-06-28 09:53:42 -0600

    create note function

---
+++ db/notes.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const notes = require("./db.json");
+
+// Create a new note
+function createNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db.json"),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return note;
+}
+
+module.exports = { createNote };
