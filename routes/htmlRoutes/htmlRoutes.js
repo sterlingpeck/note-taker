af83a0058926d48a3798828ff952c56cbfed77c5
Sterling Peck <peck.sterling@gmail.com>
2022-07-02 10:12:41 -0600

    routes

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const path = require("path");
+const notes = require("../../data/db.json");
+const { nanoid } = require("nanoid");
+const { createNewNote } = require("../../lib/notes");
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/index.html"));
@@ -11,6 +14,17 @@ router.get("/notes", (req, res) => {
 
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+router.get("/", (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+router.post("/", (req, res) => {
+  req.body.id = nanoid();
+  const note = createNewNote(req.body, notes);
+  res.json(note);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const Routes = require("./routes/htmlRoutes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,8 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use("/", Routes);
 
 app.listen(PORT, () => {
   console.log(`Server now running on port ${PORT}`);
